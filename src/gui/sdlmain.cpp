7b7662470bcff9c158c6986b35a32c17307868da
Dean Beeler <dean.beeler@oculus.com>
2022-08-15 02:34:45 -0700

    crt_lottes shader working!

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -2398,6 +2398,221 @@ static SDL_D3D11_GetTexture_fn s_GetTextureFn;
 static Microsoft::WRL::ComPtr<ID3D11ComputeShader> s_cs;
 static Microsoft::WRL::ComPtr<ID3D11SamplerState> s_ss;
 
+const char* crt_lottes = R"(
+RWTexture2D<float4> OutText : register(u0);
+Texture2D<float4> SrcText : register(t0);
+SamplerState BilinearClamp : register(s0);
+
+#define hardScan -8.0
+#define hardPix -3.0
+#define warpX 0.031
+#define warpY 0.041
+#define maskDark 0.5
+#define maskLight 1.5
+#define scaleInLinearGamma 1
+#define shadowMask 4
+#define brightboost 1
+#define hardBloomScan -2.0
+#define hardBloomPix -1.5
+#define bloomAmount 1.0/16.0
+#define shape 2.0
+
+#define DO_BLOOM 1
+
+#define warp float2(warpX,warpY)
+
+// Nearest emulated sample given floating point position and texel offset.
+// Also zero's off screen.
+float3 Fetch(float2 pos, float2 off, float2 texture_size){
+  pos=(floor(pos*texture_size.xy+off)+float2(0.5,0.5))/texture_size.xy;
+  return brightboost * SrcText.SampleLevel(BilinearClamp, pos, 0).rgb;
+}
+
+// Distance in emulated pixels to nearest texel.
+float2 Dist(float2 pos, float2 texture_size){pos=pos*texture_size.xy;return -((pos-floor(pos))-float2(0.5, 0.5));}
+    
+// 1D Gaussian.
+float Gaus(float pos,float scale){return exp2(scale*pow(abs(pos),shape));}
+
+// 3-tap Gaussian filter along horz line.
+float3 Horz3(float2 pos, float off, float2 texture_size){
+  float3 b=Fetch(pos,float2(-1.0,off),texture_size);
+  float3 c=Fetch(pos,float2( 0.0,off),texture_size);
+  float3 d=Fetch(pos,float2( 1.0,off),texture_size);
+  float dst=Dist(pos, texture_size).x;
+  // Convert distance to weight.
+  float scale=hardPix;
+  float wb=Gaus(dst-1.0,scale);
+  float wc=Gaus(dst+0.0,scale);
+  float wd=Gaus(dst+1.0,scale);
+  // Return filtered sample.
+  return (b*wb+c*wc+d*wd)/(wb+wc+wd);}
+  
+// 5-tap Gaussian filter along horz line.
+float3 Horz5(float2 pos, float off, float2 texture_size){
+  float3 a=Fetch(pos,float2(-2.0,off),texture_size);
+  float3 b=Fetch(pos,float2(-1.0,off),texture_size);
+  float3 c=Fetch(pos,float2( 0.0,off),texture_size);
+  float3 d=Fetch(pos,float2( 1.0,off),texture_size);
+  float3 e=Fetch(pos,float2( 2.0,off),texture_size);
+  float dst=Dist(pos, texture_size).x;
+  // Convert distance to weight.
+  float scale=hardPix;
+  float wa=Gaus(dst-2.0,scale);
+  float wb=Gaus(dst-1.0,scale);
+  float wc=Gaus(dst+0.0,scale);
+  float wd=Gaus(dst+1.0,scale);
+  float we=Gaus(dst+2.0,scale);
+  // Return filtered sample.
+  return (a*wa+b*wb+c*wc+d*wd+e*we)/(wa+wb+wc+wd+we);}
+
+// 7-tap Gaussian filter along horz line.
+float3 Horz7(float2 pos, float off, float2 texture_size){
+  float3 a=Fetch(pos,float2(-3.0,off),texture_size);
+  float3 b=Fetch(pos,float2(-2.0,off),texture_size);
+  float3 c=Fetch(pos,float2(-1.0,off),texture_size);
+  float3 d=Fetch(pos,float2( 0.0,off),texture_size);
+  float3 e=Fetch(pos,float2( 1.0,off),texture_size);
+  float3 f=Fetch(pos,float2( 2.0,off),texture_size);
+  float3 g=Fetch(pos,float2( 3.0,off),texture_size);
+  float dst=Dist(pos, texture_size).x;
+  // Convert distance to weight.
+  float scale=hardBloomPix;
+  float wa=Gaus(dst-3.0,scale);
+  float wb=Gaus(dst-2.0,scale);
+  float wc=Gaus(dst-1.0,scale);
+  float wd=Gaus(dst+0.0,scale);
+  float we=Gaus(dst+1.0,scale);
+  float wf=Gaus(dst+2.0,scale);
+  float wg=Gaus(dst+3.0,scale);
+  // Return filtered sample.
+  return (a*wa+b*wb+c*wc+d*wd+e*we+f*wf+g*wg)/(wa+wb+wc+wd+we+wf+wg);}
+
+// Return scanline weight.
+float Scan(float2 pos,float off, float2 texture_size){
+  float dst=Dist(pos, texture_size).y;
+  return Gaus(dst+off,hardScan);}
+  
+  // Return scanline weight for bloom.
+float BloomScan(float2 pos,float off, float2 texture_size){
+  float dst=Dist(pos, texture_size).y;
+  return Gaus(dst+off,hardBloomScan);}
+
+// Allow nearest three lines to effect pixel.
+float3 Tri(float2 pos, float2 texture_size){
+  float3 a=Horz3(pos,-1.0, texture_size);
+  float3 b=Horz5(pos, 0.0, texture_size);
+  float3 c=Horz3(pos, 1.0, texture_size);
+  float wa=Scan(pos,-1.0, texture_size);
+  float wb=Scan(pos, 0.0, texture_size);
+  float wc=Scan(pos, 1.0, texture_size);
+  return a*wa+b*wb+c*wc;}
+  
+// Small bloom.
+float3 Bloom(float2 pos, float2 texture_size){
+  float3 a=Horz5(pos,-2.0, texture_size);
+  float3 b=Horz7(pos,-1.0, texture_size);
+  float3 c=Horz7(pos, 0.0, texture_size);
+  float3 d=Horz7(pos, 1.0, texture_size);
+  float3 e=Horz5(pos, 2.0, texture_size);
+  float wa=BloomScan(pos,-2.0, texture_size);
+  float wb=BloomScan(pos,-1.0, texture_size);
+  float wc=BloomScan(pos, 0.0, texture_size);
+  float wd=BloomScan(pos, 1.0, texture_size);
+  float we=BloomScan(pos, 2.0, texture_size);
+  return a*wa+b*wb+c*wc+d*wd+e*we;}
+
+// Distortion of scanlines, and end of screen alpha.
+float2 Warp(float2 pos){
+  pos=pos*2.0-1.0;    
+  pos*=float2(1.0+(pos.y*pos.y)*warp.x,1.0+(pos.x*pos.x)*warp.y);
+  return pos*0.5+0.5;}
+
+// Shadow mask 
+float3 Mask(float2 pos){
+  float3 mask=float3(maskDark,maskDark,maskDark);
+
+  // Very compressed TV style shadow mask.
+  if (shadowMask == 1) {
+    float mask_line = maskLight;
+    float odd=0.0;
+    if(frac(pos.x/6.0)<0.5) odd = 1.0;
+    if(frac((pos.y+odd)/2.0)<0.5) mask_line = maskDark;  
+    pos.x=frac(pos.x/3.0);
+   
+    if(pos.x<0.333)mask.r=maskLight;
+    else if(pos.x<0.666)mask.g=maskLight;
+    else mask.b=maskLight;
+    mask *= mask_line;  
+  } 
+
+  // Aperture-grille.
+  else if (shadowMask == 2) {
+    pos.x=frac(pos.x/3.0);
+
+    if(pos.x<0.333)mask.r=maskLight;
+    else if(pos.x<0.666)mask.g=maskLight;
+    else mask.b=maskLight;
+  } 
+
+  // Stretched VGA style shadow mask (same as prior shaders).
+  else if (shadowMask == 3) {
+    pos.x+=pos.y*3.0;
+    pos.x=frac(pos.x/6.0);
+
+    if(pos.x<0.333)mask.r=maskLight;
+    else if(pos.x<0.666)mask.g=maskLight;
+    else mask.b=maskLight;
+  }
+
+  // VGA style shadow mask.
+  else if (shadowMask == 4) {
+    pos.xy=floor(pos.xy*float2(1.0,0.5));
+    pos.x+=pos.y*3.0;
+    pos.x=frac(pos.x/6.0);
+
+    if(pos.x<0.333)mask.r=maskLight;
+    else if(pos.x<0.666)mask.g=maskLight;
+    else mask.b=maskLight;
+  }
+
+  return mask;
+}    
+
+float4 crt_lottes(float2 texture_size, float2 video_size, float2 output_size, float2 tex)
+{
+  float2 pos=Warp(tex.xy*(texture_size.xy/video_size.xy))*(video_size.xy/texture_size.xy);
+  float3 outColor = Tri(pos, texture_size);
+
+#ifdef DO_BLOOM
+  //Add Bloom
+  outColor.rgb+=Bloom(pos, texture_size)*bloomAmount;
+#endif
+
+  if(shadowMask)
+    outColor.rgb*=Mask(floor(tex.xy*(texture_size.xy/video_size.xy)*output_size.xy)+float2(0.5,0.5));
+
+  return float4(outColor.rgb,1.0);
+}
+
+[numthreads( 8, 8, 1 )]
+void main( uint3 DTid : SV_DispatchThreadID )
+{
+	float2 OutTexDims;
+	OutText.GetDimensions(OutTexDims.x, OutTexDims.y);
+
+	float2 InTexDims;
+	SrcText.GetDimensions(InTexDims.x, InTexDims.y);
+    
+	float2 TexelSize = 1.0 / OutTexDims;
+    float2 UV = TexelSize * (DTid.xy + 0.5);
+    
+    float4 Src1 = crt_lottes(InTexDims, InTexDims, OutTexDims, UV);
+
+    OutText[DTid.xy] = Src1;
+}
+)";
+
 const char* computeShader = R"(
 RWTexture2D<float4> OutText : register(u0);
 Texture2D<float4> SrcText : register(t0);
@@ -2466,15 +2681,16 @@ static void ShadeSDLDX11()
 		ComPtr<ID3DBlob> codeBlob;
 		ComPtr<ID3DBlob> errorBlob;
 
-		D3DCompile(computeShader, strlen(computeShader), "computeShader", nullptr, nullptr, "main", "cs_5_0", 0, 0, codeBlob.GetAddressOf(), errorBlob.GetAddressOf());
+		//D3DCompile(computeShader, strlen(computeShader), "computeShader", nullptr, nullptr, "main", "cs_5_0", 0, 0, codeBlob.GetAddressOf(), errorBlob.GetAddressOf());
+		D3DCompile(crt_lottes, strlen(crt_lottes), "computeShader", nullptr, nullptr, "main", "cs_5_0", 0, 0, codeBlob.GetAddressOf(), errorBlob.GetAddressOf());
 		
 		device->CreateComputeShader(codeBlob->GetBufferPointer(), codeBlob->GetBufferSize(), nullptr, s_cs.GetAddressOf());
 
 		D3D11_SAMPLER_DESC sampDesc{};
-		sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-		sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-		sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
-		sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+		sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+		sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
+		sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
+		sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
 		sampDesc.MipLODBias = 0;
 		sampDesc.MaxAnisotropy = 1;
 		sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
